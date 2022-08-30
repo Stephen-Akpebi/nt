bafd965bc7dae5eefa759234b15731353ce4a939
Stephen-Akpebi <www.nineclouds@gmail.com>
2022-08-30 09:49:48 +0000

    tweaked app for heroku

--- native/urls.py
+++ native/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     path('timetable/',views.Timetable.as_view(),name='timetable'),
     path('bmi/',views.bmi,name='bmi'),
     path('classdetail/',views.ClassDetail.as_view(),name='classdetail'),
+    path('blog/', views.PostList.as_view(), name='blog'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='blog_detail'),
 ]
 
