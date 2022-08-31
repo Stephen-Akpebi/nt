6eff41581b1cde3a212f20a4a8b4bae5dcfa6182
Stephen-Akpebi <www.nineclouds@gmail.com>
2022-08-31 07:43:55 +0000

    tweaked app for heroku

--- native_web/settings.py
+++ native_web/settings.py
@@ -38,7 +38,7 @@ SENDGRID_API_KEY='<your-sendgrid-api-key>'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nativegym.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
