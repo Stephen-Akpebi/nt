a90f0293171b61e14a479641b4e291a790bcaa1e
Stephen-Akpebi <www.nineclouds@gmail.com>
2022-08-31 08:06:13 +0000

    tweaked app for heroku

--- native_web/settings.py
+++ native_web/settings.py
@@ -36,7 +36,7 @@ SENDGRID_API_KEY='<your-sendgrid-api-key>'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
